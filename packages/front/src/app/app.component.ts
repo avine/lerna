e361761a7cf6947befdfaf802ef0043dd77cfa31
Stéphane Francel <contact@avine.io>
2020-10-22 10:49:17 +0200

    add shared

--- packages/front/src/app/app.component.ts
+++ packages/front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SHARED } from '@locale/shared';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  title = SHARED;
 }
---
+++ packages/shared/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SHARED = void 0;
+exports.SHARED = 'SHARED!!';
+//# sourceMappingURL=index.js.map
